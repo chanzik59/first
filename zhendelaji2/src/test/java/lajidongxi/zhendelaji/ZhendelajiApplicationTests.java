137b8c314696d4022febf8972de2e2b8f633562b
chanzik59 <1320046450@qq.com>
2019-08-21 14:23:05 +0800

    create project for first git

---
+++ zhendelaji2/src/test/java/lajidongxi/zhendelaji/ZhendelajiApplicationTests.java
@@ -0,0 +1,16 @@
+package lajidongxi.zhendelaji;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZhendelajiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
